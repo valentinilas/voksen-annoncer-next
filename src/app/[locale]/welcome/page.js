9688530670050a7f8ea03999d7ac6e3ad85eb79b
Valentin Ilas <valentin.ilas@gmail.com>
2024-06-22 12:25:49 +0200

    added welcome screen for signup

---
+++ src/app/[locale]/welcome/page.js
@@ -0,0 +1,18 @@
+import Button from "@/components/button/button";
+
+import { useTranslations } from "next-intl";
+export default function Welcome() {
+    const t = useTranslations();
+
+    return (
+        <div className="container mx-auto bg-base-200  p-5 rounded-box  sm:max-w-sm">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col items-start">
+                <h2 className="text-2xl font-bold mb-4 text-center">{t("welcome.headline")}</h2>
+                <p className="mb-10">{t("welcome.body")}</p>
+                <Button variant="primary" className="" to="/">{t("welcome.homepage")}</Button>
+            </div>
+        </div>
+    );
+};
+
+
