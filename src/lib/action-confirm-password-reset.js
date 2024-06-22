bb8892a88461168c72c21b8090fec08f61d7633c
Valentin Ilas <valentin.ilas@gmail.com>
2024-06-22 14:20:44 +0200

    token exchange error

--- src/lib/action-confirm-password-reset.js
+++ src/lib/action-confirm-password-reset.js
@@ -3,22 +3,25 @@
 import { createClient } from '@/utils/supabase/server'
 
 export async function confirmPasswordReset(prevState, formData) {
-    const supabase = createClient();
-    const password = formData.get('password');
-    const code = formData.get('code');
-  
-    if (!code) {
-      return { error: 'Invalid or missing reset token' };
-    }
-    supabase.auth.exchangeCodeForSession(code);
+  const supabase = createClient();
+  const password = formData.get('password');
+  const code = formData.get('code');
 
-
-
-    const { error } = await supabase.auth.updateUser({ password });
-  
-    if (error) {
-      return { error: error.message };
-    }
-  
-    return { success: true };
+  if (!code) {
+    return { error: 'Invalid or missing reset token' };
   }
+  const { data, error: tokenExchangeError } = await supabase.auth.exchangeCodeForSession(code)
+
+  if (tokenExchangeError) {
+    return { error: error.message };
+  }
+
+
+  const { error } = await supabase.auth.updateUser({ password });
+
+  if (error) {
+    return { error: error.message };
+  }
+
+  return { success: true };
+}
