05ea1dfec4bf566492c73ff6000f525a6e0efc6e
Valentin Ilas <valentin.ilas@gmail.com>
2024-06-24 18:19:04 +0200

    default locale detection false

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,7 @@ import { NextResponse } from 'next/server';
 // Create the next-intl middleware
 const intlMiddleware = createMiddleware({
   locales: ['da', 'en'],
+  localeDetection: false,
   defaultLocale: 'da'
 });
 
