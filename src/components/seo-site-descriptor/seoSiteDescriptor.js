2244194b8ae6af343306127f1aedc0338f02b77b
Valentin Ilas <valentin.ilas@gmail.com>
2024-07-02 23:20:47 +0200

    small change to descriptor

--- src/components/seo-site-descriptor/seoSiteDescriptor.js
+++ src/components/seo-site-descriptor/seoSiteDescriptor.js
@@ -4,14 +4,20 @@ import Link from "next/link";
 export default function SeoSiteDescriptor() {
     const t = useTranslations("seo-descriptor");
     return <div className="bg-base-200 p-10 mb-5 mt-10 rounded-box shadow-xl">
-        <p className="mb-4">{t("p1")}</p>
+        <p className="mb-4">{t.rich("p1", {
+                searchLink: (chunks) => (<Link className="link" href={`/search/${chunks}`}>{chunks}</Link>
+                )
+            })}</p>
         <p className="mb-4">
             {t.rich("p2", {
                 searchLink: (chunks) => (<Link className="link" href={`/search/${chunks}`}>{chunks}</Link>
                 )
             })}
         </p>
-        <p className="mb-4">{t("p3")}</p>
+        <p className="mb-4">{t.rich("p3", {
+                searchLink: (chunks) => (<Link className="link" href={`/search/${chunks}`}>{chunks}</Link>
+                )
+            })}</p>
         <p className="mb-4">
             {t.rich("p4", {
                 emailLink: (chunks) => (<a className="link" href="mailto:support@voksen-annoncer.com">{chunks}</a>
