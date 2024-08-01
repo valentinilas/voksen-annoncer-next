d7895c543e2bd3d88772f932fe155728de8d6d7b
Valentin Ilas <valentin.ilas@gmail.com>
2024-08-01 23:39:02 +0200

    removed duplicate props

--- src/app/[locale]/articles/page.js
+++ src/app/[locale]/articles/page.js
@@ -39,7 +39,7 @@ export default async function Article({ params }) {
        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
 
         {articles.map(article=>{
-            return <Link className="link" href={`/articles/${article.slug}`} key={article.id} className="article-card">
+            return <Link  href={`/articles/${article.slug}`} key={article.id} className="article-card">
                 <div className="relative overflow-hidden bg-cover bg-no-repeat rounded-box mb-5">
                 <Image src={article.summary_image} alt={article.title} width={600} height={600} />
                 <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-cherry-200 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-20"></div></div>
