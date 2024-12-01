50560f37fb8e2c4ec98da2a0bf6479f1a8f8a9bf
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-01 15:13:20 +0100

    Fixed navbar tabindex

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -105,7 +105,7 @@ export default function NavBar() {
                     </div>
                     <ul className="menu menu-horizontal px-1 gap-2">
                         <li>
-                            <label htmlFor="my-drawer" aria-label="Open sidebar" role="button" tabindex="0" className="btn btn-circle btn-ghost">
+                            <label htmlFor="my-drawer" aria-label="Open sidebar" role="button" tabIndex="0" className="btn btn-circle btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
                         </li>
