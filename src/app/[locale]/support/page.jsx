48ace2134b2eb713d64bbc864cee09023dc19d6b
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-29 03:04:28 +0100

    FIx h1

--- src/app/[locale]/support/page.jsx
+++ src/app/[locale]/support/page.jsx
@@ -21,7 +21,7 @@ export default function Support() {
                 <h1 className="text-4xl mb-5">{t('support.title')}</h1>
                 <p className="mb-3">{t('support.contactEmail')} <a className="link" href={`mailto:${t('support.emailAddress')}`}>{t('support.emailAddress')}</a></p>
 
-                <h1 className="text-2xl mb-5 mt-5">{t('support.cookiePolicy.title')}</h1>
+                <h2 className="text-2xl mb-5 mt-5">{t('support.cookiePolicy.title')}</h2>
                 <p className="mb-3">{t('support.cookiePolicy.description')} <Link className="link" href={t('support.cookiePolicy.policyLink')}>{t('support.cookiePolicy.linkText')}</Link></p>
             </div>
 
