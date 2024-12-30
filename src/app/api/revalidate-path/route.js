5ce8d5206b37b775003b2608a4e229e64ea2c2e7
Valentin Ilas <valentin.ilas@gmail.com>
2024-12-30 13:37:15 +0100

    Revalidate

--- src/app/api/revalidate-path/route.js
+++ src/app/api/revalidate-path/route.js
@@ -1,12 +1,12 @@
 import { revalidateTag } from 'next/cache';
  
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  if (searchParams?.secret !== process.env.NEXT_REVALIDATE_PATH_TOKEN) {
-    return new Response(`Invalid credentials`, {
-      status: 500,
-    });
-  }
+//   const { searchParams } = new URL(request.url);
+//   if (searchParams?.secret !== process.env.NEXT_REVALIDATE_PATH_TOKEN) {
+//     return new Response(`Invalid credentials`, {
+//       status: 500,
+//     });
+//   }
   revalidateTag('public-posts');
 
   return new Response(
