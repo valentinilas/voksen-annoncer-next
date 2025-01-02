60008f9977404843a9d52b40ec70e6b858dcd7a1
Valentin Ilas <valentin.ilas@gmail.com>
2025-01-02 19:01:04 +0100

    Test component

---
+++ src/app/[locale]/test/components/da.js
@@ -0,0 +1,14 @@
+export default function TestPageDanish() {
+    return (
+        <div>
+            <h1 className="text-4xl mb-5">Test DA Support</h1>
+            <p className="mb-3">
+                Contactez-nous par email à <a className="link" href="mailto:support@example.com">support@example.com</a>.
+            </p>
+            <h2 className="text-2xl mb-5 mt-5">Politique des cookies</h2>
+            <p className="mb-3">
+                Lisez notre politique sur les cookies <a className="link" href="/fr/cookie-policy">ici</a>.
+            </p>
+        </div>
+    );
+}
