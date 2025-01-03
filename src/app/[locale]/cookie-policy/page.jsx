63eb08793afab5c08f711ae9febf890c56aa10a9
Valentin Ilas <valentin.ilas@gmail.com>
2025-01-03 12:15:59 +0100

    Static generation for Cookie Policy

--- src/app/[locale]/cookie-policy/page.jsx
+++ src/app/[locale]/cookie-policy/page.jsx
@@ -1,9 +1,22 @@
 import { useTranslations } from "next-intl";
 import { getTranslations } from 'next-intl/server';
+import { routing } from '@/i18n/routing';
+import { setRequestLocale } from "next-intl/server";
 
-export async function generateMetadata({ params, searchParams }, parent) {
-    const t = await getTranslations();
-    const { locale } = await params
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({ locale }));
+}
+
+
+
+
+export async function generateMetadata(props) {
+    const params = await props.params;
+    const locale = params.locale;
+
+    setRequestLocale(locale);
+    const t = await getTranslations('');
     return {
         title: `${t("navigation.cookie-policy")} | ${t("navigation.site-name")}`,
         alternates: {
@@ -17,7 +30,8 @@ export async function generateMetadata({ params, searchParams }, parent) {
 
 }
 export default function CookiePolicy() {
-    const t = useTranslations();
+
+    const t = useTranslations('');
 
     return (
         <section className="cookie-policy">
