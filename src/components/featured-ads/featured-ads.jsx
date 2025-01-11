29c259faa31a5c103ed7b563a3af6e52b2527e61
Valentin Ilas <valentin.ilas@gmail.com>
2025-01-11 18:02:00 +0100

    Fix text translations

--- src/components/featured-ads/featured-ads.jsx
+++ src/components/featured-ads/featured-ads.jsx
@@ -10,8 +10,12 @@ import {
   NextButton,
   usePrevNextButtons
 } from './embla-buttons';
+import { useTranslations } from 'next-intl';
 
 export function FeaturedAds({ featuredAds = [] }) {
+
+  const t = useTranslations();
+
   const [emblaRef, emblaApi] = useEmblaCarousel({
     align: 'start',
     containScroll: 'trim',
@@ -25,38 +29,41 @@ export function FeaturedAds({ featuredAds = [] }) {
     onNextButtonClick
   } = usePrevNextButtons(emblaApi)
 
+  if (!featuredAds || featuredAds.length === 0) {
+    return null;
+  }
   return (
     <>
-    <h2 className="text-sm mb-4">Dagens fremhævede annoncer</h2>
-    <div className="embla overflow-hidden mb-10" ref={emblaRef}>
-      <div className="embla__container grid grid-flow-col xl:auto-cols-[25%] md:auto-cols-[50%] lg:auto-cols-[33.33%] auto-cols-[100%] min-h-10">
-        {featuredAds.map((ad, index) => {
-          const { ad_images, title } = ad;
-          return (
+      <h2 className="text-sm mb-4">{t('featured.headline')}</h2>
+      <div className="embla overflow-hidden mb-10" ref={emblaRef}>
+        <div className="embla__container grid grid-flow-col xl:auto-cols-[25%] md:auto-cols-[50%] lg:auto-cols-[33.33%] auto-cols-[100%] min-h-10">
+          {featuredAds.map((ad, index) => {
+            const { ad_images, title } = ad;
+            return (
 
-            <div key={index} className="embla__slide mr-2">
-              {/* <img src={ad.uui} alt={ad.title} /> */}
-              <Link className="embla__slide-wrapper flex h-full shadow-md bg-base-200 hover:bg-base-100  px-4 py-4 gap-4 items-center rounded-box" href={`/posts/${ad.slug}`}>
-                <div className="size-16 shrink-0">
-                  {ad_images?.length > 0 ? <Image src={ad_images[0]?.image_url} width={64} height={64} className="rounded-lg w-full object-cover aspect-square bg-neutral border-base-100" alt="" /> : <DefaultImage iconSize='size-6' />}
-                </div>
-                <div>
-                  {`${ad.title.slice(0, 40)}...`}
-                </div>
+              <div key={index} className="embla__slide mr-2">
+                {/* <img src={ad.uui} alt={ad.title} /> */}
+                <Link className="embla__slide-wrapper flex h-full shadow-md bg-base-200 hover:bg-base-100  px-4 py-4 gap-4 items-center rounded-box" href={`/posts/${ad.slug}`}>
+                  <div className="size-16 shrink-0">
+                    {ad_images?.length > 0 ? <Image src={ad_images[0]?.image_url} width={64} height={64} className="rounded-lg w-full object-cover aspect-square bg-neutral border-base-100" alt="" /> : <DefaultImage iconSize='size-6' />}
+                  </div>
+                  <div>
+                    {`${ad.title.slice(0, 40)}...`}
+                  </div>
 
 
-              </Link>
-            </div>
-          )
-        })}
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+        <div className="embla__controls flex justify-center gap-2 mt-4">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} className="hover:text-cherry-500 rounded-full disabled:opacity-25" />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} className="hover:text-cherry-500 rounded-full disabled:opacity-25" />
+
+
+        </div>
       </div>
-      <div className="embla__controls flex justify-center gap-2 mt-4">
-        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} className="hover:text-cherry-500 rounded-full disabled:opacity-25" />
-        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} className="hover:text-cherry-500 rounded-full disabled:opacity-25" />
-
-
-      </div>
-    </div>
     </>
   )
 
