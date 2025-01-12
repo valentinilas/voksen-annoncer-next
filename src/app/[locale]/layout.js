6f0221c02eb7a15cd3855a2943845d50627be07c
Valentin Ilas <valentin.ilas@gmail.com>
2025-01-12 18:33:21 +0100

    lazy load the scripts

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -78,11 +78,11 @@ export default async function RootLayout(props) {
                             <Script
                                 id="cookieyes"
                                 src={`https://cdn-cookieyes.com/client_data/64376506a85904a59f66f025/script.js`}
-                                strategy="afterInteractive"
+                                strategy="lazyOnload"
                             >
                             </Script>
                             {/* <GoogleTagManager gtmId="GTM-K9P4CJ8N" /> */}
-                            <GoogleAnalytics gaId="G-JN6QV704E2" strategy="afterInteractive" />
+                            <GoogleAnalytics gaId="G-JN6QV704E2" strategy="lazyOnload" />
                             <Analytics />
                             <SpeedInsights />
                         </head>
